aa992c31365ffc0ed9c3d00a7c92ef8281baa7b0
Justis Faulkner <faulknerjustis@gmail.com>
2024-03-30 13:41:16 -0400

    Complete UI & funct for RPS

--- scripts.js
+++ scripts.js
@@ -2,6 +2,36 @@ let playerScore = 0;
 let computerScore = 0;
 let roundWinner = "";
 
+const rockBtn = document.querySelector("#rockBtn");
+const paperBtn = document.querySelector("#paperBtn");
+const scissorsBtn = document.querySelector("#scissorsBtn");
+const playerSelectionDiv = document.querySelector("#player-selection");
+const computerSelectionDiv = document.querySelector("#computer-selection");
+const roundOutcomeDiv = document.querySelector("#round-outcome");
+const scoreContainer = document.querySelector(".score-container")
+const playerScoreDiv = document.querySelector("#player-score");
+const computerScoreDiv = document.querySelector("#computer-score");
+const resetBtn = document.createElement("button");
+
+rockBtn.addEventListener("click", () => handleClick("rock"))
+paperBtn.addEventListener("click", () => handleClick("paper"))
+scissorsBtn.addEventListener("click", () => handleClick("scissors"))
+
+function handleClick(playerSelection) {
+    if (!isSeriesOver()) {
+        const computerSelection = getComputerSelection();
+        playRound(playerSelection, computerSelection);
+        updateUi(playerSelection, computerSelection);
+        if (isSeriesOver()) {
+            declareWinner();
+
+            resetBtn.textContent = "Reset";
+            scoreContainer.appendChild(resetBtn);
+            resetBtn.addEventListener("click", () => resetGame());
+        }
+    }
+}
+
 function playRound(playerSelection, computerSelection) {
     if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
@@ -22,25 +52,6 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         roundWinner = "draw"
     }
-}
-
-const rockBtn = document.querySelector("#rockBtn");
-const paperBtn = document.querySelector("#paperBtn");
-const scissorsBtn = document.querySelector("#scissorsBtn");
-const playerSelectionDiv = document.querySelector("#player-selection");
-const computerSelectionDiv = document.querySelector("#computer-selection");
-const roundOutcomeDiv = document.querySelector("#round-outcome");
-const playerScoreDiv = document.querySelector("#player-score");
-const computerScoreDiv = document.querySelector("#computer-score");
-
-rockBtn.addEventListener("click", () => handleClick("rock"))
-paperBtn.addEventListener("click", () => handleClick("paper"))
-scissorsBtn.addEventListener("click", () => handleClick("scissors"))
-
-function handleClick(playerSelection) {
-    const computerSelection = getComputerSelection();
-    playRound(playerSelection, computerSelection);
-    updateUi(playerSelection, computerSelection);
 }
 
 function getComputerSelection() {
@@ -68,24 +79,35 @@ function updateUi(playerSelection, computerSelection) {
     }
 
     playerScoreDiv.textContent = `Player Score: ${playerScore}`
-    computerScoreDiv.textContent = `Player Score: ${computerScore}`
+    computerScoreDiv.textContent = `Computer Score: ${computerScore}`
 }
 
-// function updateRoundUi(roundWinner) {
-//     if (roundWinner === "player") {
-//         roundOutcomeDiv.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
-//     } else if (roundWinner === "computer") {
-//         roundOutcomeDiv.textContent = `You lose! ${playerSelection} loses to ${computerSelection}`;
-//     } else {
-//         roundOutcomeDiv.textContent = `Draw! ${playerSelection} ties ${computerSelection}`;
-//     }
-// }
+function isSeriesOver() {
+    const winningScore = 5;
+    return (playerScore === winningScore || computerScore === winningScore)
+}
 
-// function updateScore(playerScore, computerScore) {
-//     playerScoreDiv.textContent = `Player Score: ${playerScore}`
-//     computerScoreDiv.textContent = `Player Score: ${computerScore}`
-// }
+function declareWinner() {
+    playerSelectionDiv.textContent = "";
+    computerSelectionDiv.textContent = "";
+    if (playerScore > computerScore) {
+        roundOutcomeDiv.textContent = "Congratulations, you reached 5 points and won the game!"
+    } else {
+        roundOutcomeDiv.textContent = "Better luck next time, AI beat you to 5 points!"
+    }
+}
 
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    roundWinner = ""
+    playerSelectionDiv.textContent = "";
+    computerSelectionDiv.textContent = "";
+    roundOutcomeDiv.textContent = "";
+    playerScoreDiv.textContent = "";
+    computerScoreDiv.textContent = "";
+    resetBtn.remove();
+}
 
 
 
